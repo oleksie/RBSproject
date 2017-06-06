9c61c21f2746f46e35305c58e584d95902e23a25
Mike hunt <homo@pornhub.com>
2017-06-06 10:39:08 +0200

    dingen

--- RBSsysteem/basisform/Program.cs
+++ RBSsysteem/basisform/Program.cs
@@ -16,7 +16,7 @@ namespace basisform
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new ipadbasis());
         }
     }
 }
