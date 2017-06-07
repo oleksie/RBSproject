216440281ea3edbac480637df96ba8ebbfc14b42
oleksie <alexabdalla95@gmail.com>
2017-06-07 13:44:09 +0200

    connectie met db

---
+++ RBSsysteem/DAL/DALConnection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class DALConnection
+    {
+        public SqlConnection openConnectieDB(string naam)
+        {
+            string connString = ConfigurationManager.ConnectionStrings[naam].ConnectionString;
+            SqlConnection connection = new SqlConnection(connString);
+
+            connection.Open();
+            return connection;
+        }
+
+        private void sluitConnectieDB(SqlConnection connection)
+        {
+            connection.Close();
+        }
+    }
+}
