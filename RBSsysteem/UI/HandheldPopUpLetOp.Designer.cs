2677a5b42c547c1c0f1ffd71769f55bee057fe4c
Mike hunt <homo@pornhub.com>
2017-06-07 23:28:21 +0200

    popups

---
+++ RBSsysteem/UI/HandheldPopUpLetOp.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace UI
+{
+    partial class HandheldPopUpLetOp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Btn_HandheldPopUpLetOPJa = new System.Windows.Forms.Button();
+            this.Btn_handheldPopUpLetOpNee = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(191, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(102, 32);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Let op!";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(105, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(285, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "De huidige bestelling is nog leeg";
+            // 
+            // Btn_HandheldPopUpLetOPJa
+            // 
+            this.Btn_HandheldPopUpLetOPJa.Location = new System.Drawing.Point(43, 233);
+            this.Btn_HandheldPopUpLetOPJa.Name = "Btn_HandheldPopUpLetOPJa";
+            this.Btn_HandheldPopUpLetOPJa.Size = new System.Drawing.Size(111, 64);
+            this.Btn_HandheldPopUpLetOPJa.TabIndex = 10;
+            this.Btn_HandheldPopUpLetOPJa.Text = "Ja";
+            this.Btn_HandheldPopUpLetOPJa.UseVisualStyleBackColor = true;
+            this.Btn_HandheldPopUpLetOPJa.Click += new System.EventHandler(this.Btn_HandheldPopUpBestelTerug_Click);
+            // 
+            // Btn_handheldPopUpLetOpNee
+            // 
+            this.Btn_handheldPopUpLetOpNee.Location = new System.Drawing.Point(334, 233);
+            this.Btn_handheldPopUpLetOpNee.Name = "Btn_handheldPopUpLetOpNee";
+            this.Btn_handheldPopUpLetOpNee.Size = new System.Drawing.Size(111, 64);
+            this.Btn_handheldPopUpLetOpNee.TabIndex = 9;
+            this.Btn_handheldPopUpLetOpNee.Text = "Nee";
+            this.Btn_handheldPopUpLetOpNee.UseVisualStyleBackColor = true;
+            this.Btn_handheldPopUpLetOpNee.Click += new System.EventHandler(this.Btn_HandheldPopUpBestelOk_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(163, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 24);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Weet u het zeker?";
+            // 
+            // HandheldPopUpLetOp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 337);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Btn_HandheldPopUpLetOPJa);
+            this.Controls.Add(this.Btn_handheldPopUpLetOpNee);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Name = "HandheldPopUpLetOp";
+            this.Text = "HandheldPopUpLetOp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Btn_HandheldPopUpLetOPJa;
+        private System.Windows.Forms.Button Btn_handheldPopUpLetOpNee;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ RBSsysteem/UI/HandheldPopUpReminder.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace UI
+{
+    partial class HandheldPopUpReminder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Btn_HandheldPopUpReminderTerug = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(450, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tafel word opgenomen door een andere serveerder";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(178, 37);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 32);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Reminder";
+            // 
+            // Btn_HandheldPopUpReminderTerug
+            // 
+            this.Btn_HandheldPopUpReminderTerug.Location = new System.Drawing.Point(91, 195);
+            this.Btn_HandheldPopUpReminderTerug.Name = "Btn_HandheldPopUpReminderTerug";
+            this.Btn_HandheldPopUpReminderTerug.Size = new System.Drawing.Size(313, 98);
+            this.Btn_HandheldPopUpReminderTerug.TabIndex = 5;
+            this.Btn_HandheldPopUpReminderTerug.Text = "Terug";
+            this.Btn_HandheldPopUpReminderTerug.UseVisualStyleBackColor = true;
+            // 
+            // HandheldPopUpReminder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 337);
+            this.Controls.Add(this.Btn_HandheldPopUpReminderTerug);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Name = "HandheldPopUpReminder";
+            this.Text = "HandheldPopUpReminder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button Btn_HandheldPopUpReminderTerug;
+    }
+}
