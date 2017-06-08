31ea4d3032335f037c45fbac2ef976bc19b74b48
Anne <Anne@ANNE>
2017-06-08 14:31:08 +0200

    Status naar int

--- RBSsysteem/Model/BestellingItem.cs
+++ RBSsysteem/Model/BestellingItem.cs
@@ -11,7 +11,7 @@ namespace Model
         public MenuItem menuitem;
         public int aantal;
         public string commentaar;
-        public string status;
+        public int status;
         public DateTime tijdOpgenomen;
 
         public BestellingItem()
