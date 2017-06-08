28cb1693f015a1473001e64169dea6c4811197a4
Anne <Anne@ANNE>
2017-06-08 15:58:12 +0200

    BestellingITemDAO heeft getall methode (die niet echt alles get)

--- RBSsysteem/DAL/BestellingItemDAO.cs
+++ RBSsysteem/DAL/BestellingItemDAO.cs
@@ -3,10 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model;
+using DAL;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace DAL
 {
-    class BestellingItemDAO
+    public class BestellingItemDAO
     {
+        DALConnection dbConnection = new DALConnection();
+       
+        public BestellingItemDAO()
+        {
+
+        }
+        public List<BestellingItem> GetAll()
+        {
+            SqlConnection connection = dbConnection.openConnectieDB("reader");
+            List<BestellingItem> bestellingItemList = new List<BestellingItem>();
+
+            connection.Open();
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("Select * From BestelItem");
+
+            String sql = sb.ToString();
+
+            SqlCommand command = new SqlCommand(sql, connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                int bestellingID = reader.GetInt32(0);
+                int menuitemID = reader.GetInt32(1);
+                string opmerkingen = reader.GetString(2);
+                int aantal = reader.GetInt32(3);
+                int status = reader.GetInt32(4);
+                DateTime tijdOpgenomen = reader.GetDateTime(5);
+
+
+
+                BestellingItem bestellingitem = new BestellingItem();
+
+                //bestellingitem.menuitem = menuitemID;
+                bestellingitem.commentaar = opmerkingen;
+                bestellingitem.aantal = aantal;
+                bestellingitem.status = status;
+                bestellingitem.tijdOpgenomen = tijdOpgenomen;
+                
+
+                bestellingItemList.Add(bestellingitem);
+            }
+
+            connection.Close();
+            reader.Close();
+            return bestellingItemList;
+        }
     }
 }
