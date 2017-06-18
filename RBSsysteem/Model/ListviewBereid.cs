c1dea3d4d42dcd41e80eba9fa6e5aa8a6528f849
BuildTools <unconfigured@null.spigotmc.org>
2017-06-18 23:54:27 +0200

    420 blaze it

---
+++ RBSsysteem/Model/ListviewBereid.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class ListviewBereid
+    {
+        public string naam;
+        public int aantal;
+        public DateTime tijdOpgenomen;
+
+        public ListviewBereid()
+        {
+
+        }
+
+        public ListviewBereid(string naam, int aantal, DateTime tijdOpgenomen)
+        {
+            this.naam = naam;
+            this.aantal = aantal;
+            this.tijdOpgenomen = tijdOpgenomen;
+        }
+    }
+}
